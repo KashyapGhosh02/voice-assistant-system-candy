04d5a1c4d551e9db26ece9473e614ac3e40b4da1
kashyapghosh02 <kashyap.ghosh2002gmail.com git config --global user.email kashyap.ghosh2002gmail.com>
2024-03-07 08:26:44 +0000

    Modified Predict_functions

--- Fucntions/prediction_func.py
+++ Fucntions/prediction_func.py
@@ -4,57 +4,61 @@ logging.disable(logging.WARNING)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disabling warnings for gpu requirements
 
 import nltk
+# Download NLTK resources
+#nltk.download('punkt')
+#nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-
-# Download NLTK resources
-nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
 ps = PorterStemmer()
+
+
+
 import json 
 try:
     print("Uploading....")
-    with open(r'Data\intents.json') as file:
+    with open(r"/home/kg/project/voice-assistant-system-candy/Data/intents.json") as file:
         intents= json.load(file)
         print("JSON file upload done")
 except FileNotFoundError as e:
     print(e)
+
+try:
+    with open(r"/home/kg/project/voice-assistant-system-candy/Data/tokenizer.pickle","rb") as file_tockenizer:
+        tokenizer=load(file_tockenizer)
+except FileNotFoundError as e:
+    print(e)
+#loading label encoder
+try:
+    with open(r"/home/kg/project/voice-assistant-system-candy/Data/label_encoder.pickle","rb") as enc:
+        lbl_encoder=load(enc)
+except FileNotFoundError as e:
+    print(e)
+
+import tensorflow as tf    
+#loading the saved model
+try:
+    # load trained model
+    model = load_model("/home/kg/project/voice-assistant-system-candy/Data/model_trained.h5")
+    print("Model loaded successfully.")
+except Exception as e:
+    print(f"Error loading the model: {e}")
+
 
 from keras_preprocessing.sequence import pad_sequences
 import numpy as np
 #from keras.models import load_model
 from pickle import load
 from tensorflow.python.keras.models import load_model
-from keras.preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+#from keras.preprocessing.text import Tokenizer
+#from keras_preprocessing.sequence import pad_sequences
+#from sklearn.preprocessing import LabelEncoder
+#from tensorflow.python.keras.models import Sequential
+#from tensorflow.python.keras.layers import Dense, Embedding, GlobalAveragePooling1D
 
-import tensorflow as tf 
 
 #loading the tokenizer
 
-try:
-    with open(r'Data\tokenizer.pickle',"rb") as file_tockenizer:
-        tokenizer=load(file_tockenizer)
-except FileNotFoundError as e:
-    print(e)
-#loading label encoder
-try:
-    with open(r'Data\label_encoder.pickle',"rb") as enc:
-        lbl_encoder=load(enc)
-except FileNotFoundError as e:
-    print(e)
-    
-#loading the saved model
-#loading the saved model
-try:
-    # load trained model
-    model = load_model('Data\chat_model')
-    print("Model loaded successfully.")
-except Exception as e:
-    print(f"Error loading the model: {e}")
  
 #prediction fucntion    
 def predict_intent_with_nltk(model, tokenizer, label_encoder, text, max_len, ps, stop_words):
@@ -78,7 +82,8 @@ def response_generator():
     pass
 
 # Example usage:
+max_len=20
 text_to_predict = "hi candy can you recommend me  movies"
-text_len=len(text_to_predict)
+text_len=min(max_len,len(text_to_predict))
 predicted_intent = predict_intent_with_nltk(model, tokenizer, lbl_encoder, text_to_predict, text_len, ps, stop_words)
 print(f"Predicted Intent: {predicted_intent}")
